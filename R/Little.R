af47356f80142421be03d5a6045cc29329b33db1
Katie Will <KatieWill@kwills-air.student.iastate.edu>
2017-03-30 17:01:02 -0500

    initial commit of data documentation

--- R/Little.R
+++ R/Little.R
@@ -5,7 +5,19 @@
 #' @examples
 #' ave_chick_wt()
 
-
+#' Prices of 50,000 round cut diamonds.
+#'
+#' A dataset containing the prices and other attributes of almost 54,000
+#' diamonds.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{price, in US dollars}
+#'   \item{carat}{weight of the diamond, in carats}
+#'   ...
+#' }
+#' @source \url{http://www.diamondse.info/}
+"chicks"
 
 data("chicks")
 
