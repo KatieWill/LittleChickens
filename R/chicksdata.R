1c43bec1f76a8830c49f8a5bca23ffc45b61a877
xmzhang <zhang@zhangdembp.student.iastate.edu>
2017-03-31 12:28:14 -0500

    fix the loading data problem. Created folder "man", changed "chicks.Rda" to "chicks.rda", separated data and function into two R files.

---
+++ R/chicksdata.R
@@ -0,0 +1,12 @@
+#' Average weight of 28 chicks.
+#'
+#' A dataset containing the weights of 28 chicks.
+#'
+#' @format A data frame with 53940 rows and 10 variables:
+#' \describe{
+#'   \item{price}{weight, in grams}
+#'   \item{Chick}{identifier}
+#'   ...
+#' }
+#' @source \url{https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/ChickWeight.html}
+"chicks"
